ae93980c62a68d3ccb3fbeb42a9d4b4d6de653e2
Henrik Hansen <hhansen06@googlemail.com>
2023-12-29 13:56:45 +0100

    Projektdateien hinzufügen.

---
+++ DroneUploader/Form1.Designer.vb
@@ -0,0 +1,308 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Label1 = New Label()
+        TextBox1 = New TextBox()
+        GroupBox1 = New GroupBox()
+        ListBox1 = New ListBox()
+        FolderBrowserDialog1 = New FolderBrowserDialog()
+        Button2 = New Button()
+        classify_files = New ComponentModel.BackgroundWorker()
+        Label2 = New Label()
+        Button3 = New Button()
+        GroupBox2 = New GroupBox()
+        ComboBox2 = New ComboBox()
+        Label5 = New Label()
+        ComboBox1 = New ComboBox()
+        Label4 = New Label()
+        GroupBox3 = New GroupBox()
+        Label8 = New Label()
+        Label7 = New Label()
+        Label6 = New Label()
+        Label3 = New Label()
+        ProgressBar1 = New ProgressBar()
+        ProgressBar2 = New ProgressBar()
+        upload_files = New ComponentModel.BackgroundWorker()
+        GroupBox1.SuspendLayout()
+        GroupBox2.SuspendLayout()
+        GroupBox3.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
+        Button1.Location = New Point(6, 26)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(1902, 29)
+        Button1.TabIndex = 0
+        Button1.Text = "Quell Ordner auswählen ..."
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(6, 65)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(91, 20)
+        Label1.TabIndex = 1
+        Label1.Text = "Quellordner:"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
+        TextBox1.Location = New Point(103, 62)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(1805, 27)
+        TextBox1.TabIndex = 2
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
+        GroupBox1.Controls.Add(TextBox1)
+        GroupBox1.Controls.Add(Button1)
+        GroupBox1.Controls.Add(Label1)
+        GroupBox1.Location = New Point(12, 12)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(1920, 106)
+        GroupBox1.TabIndex = 3
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Quelle"
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 20
+        ListBox1.Location = New Point(6, 101)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(1902, 164)
+        ListBox1.TabIndex = 5
+        ' 
+        ' Button2
+        ' 
+        Button2.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
+        Button2.Location = New Point(6, 54)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(1902, 29)
+        Button2.TabIndex = 6
+        Button2.Text = "Bilder finden"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' classify_files
+        ' 
+        classify_files.WorkerReportsProgress = True
+        ' 
+        ' Label2
+        ' 
+        Label2.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left
+        Label2.AutoSize = True
+        Label2.Location = New Point(6, 286)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(18, 20)
+        Label2.TabIndex = 7
+        Label2.Text = "..."
+        ' 
+        ' Button3
+        ' 
+        Button3.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
+        Button3.Enabled = False
+        Button3.Location = New Point(6, 26)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(1902, 29)
+        Button3.TabIndex = 8
+        Button3.Text = "Hochladen"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        GroupBox2.Controls.Add(ComboBox2)
+        GroupBox2.Controls.Add(Label5)
+        GroupBox2.Controls.Add(ComboBox1)
+        GroupBox2.Controls.Add(Label4)
+        GroupBox2.Controls.Add(ListBox1)
+        GroupBox2.Controls.Add(Button2)
+        GroupBox2.Controls.Add(Label2)
+        GroupBox2.Location = New Point(12, 124)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(1920, 309)
+        GroupBox2.TabIndex = 9
+        GroupBox2.TabStop = False
+        GroupBox2.Text = "Identifizieren"
+        ' 
+        ' ComboBox2
+        ' 
+        ComboBox2.DropDownStyle = ComboBoxStyle.DropDownList
+        ComboBox2.FormattingEnabled = True
+        ComboBox2.Items.AddRange(New Object() {"Nicht hochladen", "InfraredCamera", "WideCamera", "ZoomCamera"})
+        ComboBox2.Location = New Point(343, 20)
+        ComboBox2.Name = "ComboBox2"
+        ComboBox2.Size = New Size(151, 28)
+        ComboBox2.TabIndex = 11
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(252, 23)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(85, 20)
+        Label5.TabIndex = 10
+        Label5.Text = "Viewquelle:"
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.DropDownStyle = ComboBoxStyle.DropDownList
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Items.AddRange(New Object() {"Nicht hochladen", "InfraredCamera", "WideCamera", "ZoomCamera"})
+        ComboBox1.Location = New Point(95, 20)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(151, 28)
+        ComboBox1.TabIndex = 9
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(6, 23)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(83, 20)
+        Label4.TabIndex = 8
+        Label4.Text = "Ortoquelle:"
+        ' 
+        ' GroupBox3
+        ' 
+        GroupBox3.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        GroupBox3.Controls.Add(Label8)
+        GroupBox3.Controls.Add(Label7)
+        GroupBox3.Controls.Add(Label6)
+        GroupBox3.Controls.Add(Label3)
+        GroupBox3.Controls.Add(ProgressBar1)
+        GroupBox3.Controls.Add(ProgressBar2)
+        GroupBox3.Controls.Add(Button3)
+        GroupBox3.Location = New Point(12, 439)
+        GroupBox3.Name = "GroupBox3"
+        GroupBox3.Size = New Size(1920, 174)
+        GroupBox3.TabIndex = 10
+        GroupBox3.TabStop = False
+        GroupBox3.Text = "Hochladen"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(1750, 142)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(18, 20)
+        Label8.TabIndex = 15
+        Label8.Text = "..."
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(1750, 87)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(18, 20)
+        Label7.TabIndex = 14
+        Label7.Text = "..."
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(6, 61)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(177, 20)
+        Label6.TabIndex = 13
+        Label6.Text = "Fortschritt einzelne Datei:"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(6, 116)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(132, 20)
+        Label3.TabIndex = 11
+        Label3.Text = "Fortschritt Gesamt:"
+        ' 
+        ' ProgressBar1
+        ' 
+        ProgressBar1.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
+        ProgressBar1.Location = New Point(6, 139)
+        ProgressBar1.Name = "ProgressBar1"
+        ProgressBar1.Size = New Size(1738, 29)
+        ProgressBar1.TabIndex = 9
+        ' 
+        ' ProgressBar2
+        ' 
+        ProgressBar2.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
+        ProgressBar2.Location = New Point(6, 84)
+        ProgressBar2.Name = "ProgressBar2"
+        ProgressBar2.Size = New Size(1738, 29)
+        ProgressBar2.TabIndex = 12
+        ' 
+        ' upload_files
+        ' 
+        upload_files.WorkerReportsProgress = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1940, 625)
+        Controls.Add(GroupBox3)
+        Controls.Add(GroupBox2)
+        Controls.Add(GroupBox1)
+        Name = "Form1"
+        Text = "Form1"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        GroupBox2.ResumeLayout(False)
+        GroupBox2.PerformLayout()
+        GroupBox3.ResumeLayout(False)
+        GroupBox3.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents Button2 As Button
+    Friend WithEvents classify_files As System.ComponentModel.BackgroundWorker
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents upload_files As System.ComponentModel.BackgroundWorker
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents ProgressBar2 As ProgressBar
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+End Class
---
+++ DroneUploader/Form1.vb
@@ -0,0 +1,316 @@
+﻿Imports System.ComponentModel
+Imports System.Globalization
+Imports System.IO
+Imports System.Security.Cryptography
+Imports System.Text
+Imports MetadataExtractor
+Imports MetadataExtractor.Formats.Xmp
+Imports System.Net.Http
+Imports System.Net.Http.Handlers
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If (FolderBrowserDialog1.ShowDialog() = DialogResult.OK) Then
+            My.Settings.src_ordner = FolderBrowserDialog1.SelectedPath
+            My.Settings.Save()
+            TextBox1.Text = FolderBrowserDialog1.SelectedPath
+        End If
+        Button2.Enabled = True
+        ListBox1.Items.Clear()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox1.Text = My.Settings.src_ordner
+        If Not File.Exists("uploaded_files.txt") Then
+            ' Die Datei erstellen
+            File.Create("uploaded_files.txt").Close()
+        End If
+
+        ComboBox1.SelectedIndex = My.Settings.orto_src
+        ComboBox2.SelectedIndex = My.Settings.view_src
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ListBox1.Items.Clear()
+        classify_files.RunWorkerAsync(TextBox1.Text)
+        Button2.Enabled = False
+    End Sub
+    Private Sub classify_files_DoWork(sender As Object, e As DoWorkEventArgs) Handles classify_files.DoWork
+
+        Dim return_files As New List(Of DroneImage)
+        Dim anz_orto As Integer = 0
+        Dim anz_view As Integer = 0
+        Dim anz_sum As Integer = 0
+
+        Dim myfiles = System.IO.Directory.GetFiles(e.Argument, "*.JPG", System.IO.SearchOption.AllDirectories)
+        For Each file In myfiles
+            If CheckIfLineExists(Path.GetFileName(file)) Then
+                classify_files.ReportProgress(0, file + " - bereits hochgeladen.")
+            Else
+                Dim image As DroneImage = GetExifData(file)
+
+                If image.ImageSource = ComboBox2.Items.Item(My.Settings.view_src) Then
+                    classify_files.ReportProgress(0, file + " - G.Roll " + image.GimbalRollDegree.ToString + " G.Yaw: " + image.GimbalYawDegree.ToString + " G.Pitch: " + image.GimbalPitchDegree.ToString + "SRC: " + image.ImageSource)
+                    return_files.Add(image)
+                    anz_view = anz_view + 1
+                End If
+
+                If image.ImageSource = ComboBox1.Items.Item(My.Settings.orto_src) Then
+                    If image.isOrtogonal Then
+                        classify_files.ReportProgress(0, file + " - G.Roll " + image.GimbalRollDegree.ToString + " G.Yaw: " + image.GimbalYawDegree.ToString + " G.Pitch: " + image.GimbalPitchDegree.ToString + "SRC: " + image.ImageSource + " ortogonal")
+                        anz_orto = anz_orto + 1
+                        return_files.Add(image)
+                    End If
+                End If
+            End If
+        Next
+        e.Result = return_files
+        classify_files.ReportProgress(0, "Bildersuche abgeschlossen. Bilder im Ordner: " + myfiles.Count.ToString + " davon Ortogonal: " + anz_orto.ToString + " davon View: " + anz_view.ToString)
+    End Sub
+
+    Private Sub classify_files_ProgressChanged(sender As Object, e As ProgressChangedEventArgs) Handles classify_files.ProgressChanged
+        ListBox1.Items.Insert(0, e.UserState)
+    End Sub
+
+    Dim uploadFiles As List(Of DroneImage)
+
+    Private Sub classify_files_RunWorkerCompleted(sender As Object, e As RunWorkerCompletedEventArgs) Handles classify_files.RunWorkerCompleted
+        uploadFiles = e.Result
+        Button3.Enabled = True
+    End Sub
+
+    Function GetExifData(imagePath As String) As DroneImage
+        Dim xmpDirectory = ImageMetadataReader.ReadMetadata(imagePath).OfType(Of XmpDirectory)().FirstOrDefault()
+        Dim retImage As New DroneImage
+        For Each [property] In xmpDirectory.XmpMeta.Properties
+            Select Case [property].Path
+                Case "xmp:CreateDate"
+                    retImage.CreateDate = [property].Value
+                Case "drone-dji:ImageSource"
+                    retImage.ImageSource = [property].Value
+                Case "drone-dji:GpsLatitude"
+                    retImage.GpsLatitude = Double.Parse([property].Value, NumberStyles.Float, CultureInfo.InvariantCulture)
+                Case "drone-dji:GpsLongitude"
+                    retImage.GpsLongitude = Double.Parse([property].Value, NumberStyles.Float, CultureInfo.InvariantCulture)
+                Case "drone-dji:AbsoluteAltitude"
+                    retImage.AbsoluteAltitude = Double.Parse([property].Value, NumberStyles.Float, CultureInfo.InvariantCulture)
+                Case "drone-dji:RelativeAltitude"
+                    retImage.RelativeAltitude = Double.Parse([property].Value, NumberStyles.Float, CultureInfo.InvariantCulture)
+                Case "drone-dji:GimbalRollDegree"
+                    retImage.GimbalRollDegree = Double.Parse([property].Value, NumberStyles.Float, CultureInfo.InvariantCulture)
+                Case "drone-dji:GimbalYawDegree"
+                    retImage.GimbalYawDegree = Double.Parse([property].Value, NumberStyles.Float, CultureInfo.InvariantCulture)
+                Case "drone-dji:GimbalPitchDegree"
+                    retImage.GimbalPitchDegree = Double.Parse([property].Value, NumberStyles.Float, CultureInfo.InvariantCulture)
+                Case "drone-dji:FlightRollDegree"
+                    retImage.FlightRollDegree = Double.Parse([property].Value, NumberStyles.Float, CultureInfo.InvariantCulture)
+                Case "drone-dji:FlightYawDegree"
+                    retImage.FlightYawDegree = Double.Parse([property].Value, NumberStyles.Float, CultureInfo.InvariantCulture)
+                Case "drone-dji:CameraSerialNumber"
+                    retImage.CameraSerialNumber = [property].Value
+                Case "drone-dji:DroneSerialNumber"
+                    retImage.DroneSerialNumber = [property].Value
+            End Select
+        Next
+        retImage.filePath = imagePath
+        retImage.FileHash = GenerateFileHash(imagePath)
+
+        If retImage.GimbalPitchDegree < -87 Then
+            retImage.isOrtogonal = True
+        End If
+        Return retImage
+    End Function
+
+    Function GenerateFileHash(filePath As String) As String
+        Try
+            Using fileStream As FileStream = New FileStream(filePath, FileMode.Open, FileAccess.Read)
+                Using md5 As MD5 = MD5.Create()
+                    ' Hash-Wert berechnen
+                    Dim hashBytes As Byte() = md5.ComputeHash(fileStream)
+
+                    ' Byte-Array in eine Zeichenfolge umwandeln
+                    Dim sb As StringBuilder = New StringBuilder()
+                    For i As Integer = 0 To hashBytes.Length - 1
+                        sb.Append(hashBytes(i).ToString("x2"))
+                    Next
+
+                    Return sb.ToString()
+                End Using
+            End Using
+        Catch ex As Exception
+            ' Fehler behandeln
+            Console.WriteLine("Fehler beim Generieren des Hash-Werts: " & ex.Message)
+            Return Nothing
+        End Try
+    End Function
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        upload_files.RunWorkerAsync(uploadFiles)
+        Button3.Enabled = False
+        ProgressBar1.Enabled = True
+        ProgressBar1.Maximum = uploadFiles.Count
+        ProgressBar2.Enabled = True
+        ProgressBar2.Maximum = 100
+    End Sub
+
+    Private Sub upload_files_DoWork(sender As Object, e As DoWorkEventArgs) Handles upload_files.DoWork
+        Dim uploadImages As List(Of DroneImage) = e.Argument
+        Dim pos As Integer = 1
+
+        For Each image In uploadImages
+            uploadFile(image)
+            Dim retdata As New bgw_upload_status
+            retdata.pos = pos
+            retdata.action = "progress"
+            upload_files.ReportProgress(0, retdata)
+            pos = pos + 1
+        Next
+
+        If uploadImages.Count() > 0 Then
+            '     Dim responseText As String = PerformHttpGetRequest("https://api.h2com.eu/api/feuerwehr/droneupload/" + formattedDate)
+        End If
+
+    End Sub
+    Private WithEvents progressMessageHandler As New ProgressMessageHandler(New HttpClientHandler())
+    Private httpClient As New HttpClient(progressMessageHandler)
+
+    Async Sub uploadFile(image As DroneImage)
+        Dim retdata As New bgw_upload_status
+
+        Using content As New MultipartFormDataContent()
+
+            content.Add(New StringContent(image.FileHash), "FileHash")
+            content.Add(New StringContent(image.CreateDate.ToString("yyyy-MM-dd H:mm:ss")), "CreateDate")
+            content.Add(New StringContent(image.ImageSource), "ImageSource")
+            content.Add(New StringContent(image.GpsLatitude), "GpsLatitude")
+            content.Add(New StringContent(image.GpsLongitude), "GpsLongitude")
+            content.Add(New StringContent(image.AbsoluteAltitude), "AbsoluteAltitude")
+            content.Add(New StringContent(image.RelativeAltitude), "RelativeAltitude")
+            content.Add(New StringContent(image.GimbalRollDegree), "GimbalRollDegree")
+            content.Add(New StringContent(image.GimbalYawDegree), "GimbalYawDegree")
+            content.Add(New StringContent(image.GimbalPitchDegree), "GimbalPitchDegree")
+            content.Add(New StringContent(image.FlightRollDegree), "FlightRollDegree")
+            content.Add(New StringContent(image.FlightYawDegree), "FlightYawDegree")
+            content.Add(New StringContent(image.CameraSerialNumber), "CameraSerialNumber")
+            content.Add(New StringContent(image.DroneSerialNumber), "DroneSerialNumber")
+
+            Dim fileContent As New ByteArrayContent(System.IO.File.ReadAllBytes(image.filePath))
+            fileContent.Headers.ContentDisposition = New System.Net.Http.Headers.ContentDispositionHeaderValue("form-data") With {
+                .Name = "image",
+                .FileName = System.IO.Path.GetFileName(image.filePath)
+                }
+            content.Add(fileContent)
+
+            Dim url As String = My.Settings.server_url & "/api/drone/image"
+            Dim response = httpClient.PostAsync(url, content)
+
+            If response.Result.IsSuccessStatusCode Then
+                retdata.msg = "POST-Request erfolgreich. (" + response.Result.Content.ReadAsStringAsync.Result + ")"
+                '    AppendLineToFile(Path.GetFileName(image.filePath))
+            Else
+                retdata.msg = "Fehler beim POST-Request. Statuscode: (" + response.Result.Content.ReadAsStringAsync.Result + ") URL: " & url
+            End If
+            retdata.action = "log"
+            upload_files.ReportProgress(0, retdata)
+        End Using
+        '   
+    End Sub
+
+    Private Sub HttpClient_SendProgress(sender As Object, e As HttpProgressEventArgs) Handles progressMessageHandler.HttpSendProgress
+        Dim retdata As New bgw_upload_status
+        retdata.action = "progress1"
+        retdata.pos = e.ProgressPercentage
+        upload_files.ReportProgress(0, retdata)
+    End Sub
+
+    Private Sub upload_files_ProgressChanged(sender As Object, e As ProgressChangedEventArgs) Handles upload_files.ProgressChanged
+
+        Dim retdata As bgw_upload_status = e.UserState
+
+        If retdata.action = "progress" Then
+            Label7.Text = retdata.pos.ToString + "/" + ProgressBar1.Maximum.ToString
+            ProgressBar1.Value = retdata.pos
+        End If
+        If retdata.action = "progress1" Then
+            ProgressBar2.Value = retdata.pos
+            Label8.Text = retdata.pos.ToString + "/" + ProgressBar2.Maximum.ToString
+        End If
+        If retdata.action = "log" Then
+            ListBox1.Items.Insert(0, retdata.msg)
+        End If
+
+
+    End Sub
+
+
+
+
+    Private Sub upload_files_RunWorkerCompleted(sender As Object, e As RunWorkerCompletedEventArgs) Handles upload_files.RunWorkerCompleted
+        ListBox1.Items.Insert(0, "Upload abgeschlossen!")
+    End Sub
+
+    Sub AppendLineToFile(ByVal line As String)
+        ' Verwenden von StreamWriter mit Append-Option zum Anhängen einer Zeile an eine Datei
+        Using writer As New StreamWriter("uploaded_files.txt", True)
+            writer.WriteLine(line)
+        End Using
+    End Sub
+    Function CheckIfLineExists(ByVal lineToCheck As String) As Boolean
+        ' Überprüfen, ob die Zeile in der Datei vorhanden ist
+        Using reader As New StreamReader("uploaded_files.txt")
+            While Not reader.EndOfStream
+                Dim currentLine As String = reader.ReadLine()
+                If currentLine = lineToCheck Then
+                    ' Die Zeile wurde gefunden
+                    Return True
+                End If
+            End While
+        End Using
+
+        ' Die Zeile wurde nicht gefunden
+        Return False
+    End Function
+
+
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        My.Settings.orto_src = ComboBox1.SelectedIndex
+        My.Settings.Save()
+    End Sub
+
+    Private Sub ComboBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox2.SelectedIndexChanged
+        My.Settings.view_src = ComboBox2.SelectedIndex
+        My.Settings.Save()
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+End Class
+
+
+Class bgw_upload_status
+    Property action As String
+    Property msg As String
+    Property pos As Integer
+End Class
+
+Public Class DroneImage
+    Property filePath As String
+    Property FileHash As String
+    Property CreateDate As DateTime
+    Property ImageSource As String
+    Property GpsLatitude As Double
+    Property GpsLongitude As Double
+    Property AbsoluteAltitude As Double
+    Property RelativeAltitude As Double
+    Property GimbalRollDegree As Double
+    Property GimbalYawDegree As Double
+    Property GimbalPitchDegree As Double
+    Property FlightRollDegree As Double
+    Property FlightYawDegree As Double
+    Property CameraSerialNumber As String
+    Property DroneSerialNumber As String
+    Property isOrtogonal As Boolean = False
+
+End Class
